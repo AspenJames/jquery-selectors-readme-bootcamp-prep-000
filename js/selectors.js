70a587c7ed1cbff72c19c8403bcbd3c40008437d
Aspen James <aspenjames@tqca.org>
2018-01-29 22:20:48 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,19 @@
 function paragraphSelector(){
   return $("p");
 }
+
+function lastImageSelector(){
+  return $("img:last");
+}
+
+function ninjaBabySelector(){
+  return $("#baby-ninja");
+}
+
+function divSelector(){
+  $("div.pics");
+}
+
+function firstListItem(){
+  return $("ul.pic-list:first-child");
+}
